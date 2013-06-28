9a53c198252bdb23f090d6578637731c75340205
Jonathan Wagener <jonathanw@amoebasys.biz>
2013-06-28 18:06:13 +0200

    It's working \o/

--- templates/page--user--login.tpl.php
+++ templates/page--user--login.tpl.php
@@ -1,22 +1,31 @@
-<div id="auth_box">
-  <div id="header">
-    <h1 id="logo"><?php print l($site_name, '<front>'); ?></h1>
+<div id="auth_box" class="login">
+  <div id="top_part">
+    <h1 id="the_logo">
+      <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>">
+    </h1>
   </div>
 
-  <div id="main" role="main">
-    <h2><?php print $title; ?></h2>
+  <div id="middle_part">
+    <h2 class="title"><?php print $title; ?></h2>
+
     <?php print $messages; ?>
+    
     <?php print render($page['content']); ?>
   </div>
 
-  <div id="footer">
-    <div><?php print l(t('Forgot your password?'), 'user/password'); ?></div>
+  <div id="bottom_part">
+    <div class="password_link">
+      <?php print l(t('Forgot your password?'), 'user/password'); ?>
+    </div>
 
     <?php if (variable_get('user_register')): ?>
-    <div><?php print l(t('Register a new account'), 'user/register'); ?></div>
+    <div class="register_link">
+      <?php print l(t('Register a new account'), 'user/register'); ?>
+    </div>
     <?php endif; ?>
-    
-    <p></p>
-    <div><a href="<?php print url('<front>'); ?>">&larr; Back to the home page</a></div>
+
+    <div class="back_link">
+      <a href="<?php print url('<front>'); ?>">&larr; Back <?php print $site_name; ?></a>
+    </div>
   </div>
 </div>
--- templates/page--user--register.tpl.php
+++ templates/page--user--register.tpl.php
@@ -1,19 +1,29 @@
-<div id="auth_box">
-  <div id="header">
-    <h1 id="logo"><?php print l($site_name, '<front>'); ?></h1>
+<div id="auth_box" class="register">
+  <div id="top_part">
+    <h1 id="the_logo">
+      <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>">
+    </h1>
   </div>
 
-  <div id="main" role="main">
-    <h2><?php print $title; ?></h2>
+  <div id="middle_part">
+    <h2 class="title"><?php print $title; ?></h2>
+
     <?php print $messages; ?>
+    
     <?php print render($page['content']); ?>
   </div>
 
-  <div id="footer">
-    <div><?php print l(t('Login'), 'user/login'); ?></div>
-    <div><?php print l(t('Forgot your password?'), 'user/password'); ?></div>
-    
-    <p></p>
-    <div><a href="<?php print url('<front>'); ?>">&larr; Back to the home page</a></div>
+  <div id="bottom_part">
+    <div class="login_link">
+      <?php print l(t('Login'), 'user/login'); ?>
+    </div>
+
+    <div class="password_link">
+      <?php print l(t('Forgot your password?'), 'user/password'); ?>
+    </div>
+
+    <div class="back_link">
+      <a href="<?php print url('<front>'); ?>">&larr; Back <?php print $site_name; ?></a>
+    </div>
   </div>
 </div>
