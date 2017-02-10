4a6b413ff82c6e6bb5c0906c2fbeacdcd26452f4
Yogesh Pawar <yogesh.pawar01@gmail.com>
2017-02-10 17:02:56 +0530

    Pareview.sh issues solved.

--- src/EventSubscriber/BetterLoginSubscriber.php
+++ src/EventSubscriber/BetterLoginSubscriber.php
@@ -7,8 +7,19 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Better Login Subscriber class.
+ */
 class BetterLoginSubscriber implements EventSubscriberInterface {
 
+  /**
+   * Function checkForRedirection.
+   *
+   *   Redirection for anonymous users.
+   *
+   * @param GetResponseEvent $event
+   *   GetResponseEvent event.
+   */
   public function checkForRedirection(GetResponseEvent $event) {
     if (\Drupal::currentUser()->isAnonymous()) {
       // Anonymous user.
